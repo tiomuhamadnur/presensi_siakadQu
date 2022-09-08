c1632bf2f203614fb0053d116ec63c73f1ebc497
Fiki <fikiirmansyah@gmail.com>
2022-09-09 06:42:53 +0700

    fix

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -19,7 +19,6 @@ class CourseController extends Controller
     use Utils;
     public function index(Request $req)
     {
-        return "cek";
         $courses = TblCourses::where('teacher_id', Auth::user()->id)->with(['class.teacherGuider', 'transCourses'])->paginate(20);
         return CourseResource::collection($courses);
     }
